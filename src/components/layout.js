093cf2e7172b70918bf2a21ccd2e9da9ef1eee2d
Tristan Crain <tcrain@metbiblechurch.local>
2022-10-25 13:07:53 -0400

    added responsive design as well as changed the layout and color palette

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,10 @@
 import * as React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Layout({ children }) {
-
   const [showSidebar, setShowSidebar] = useState(false);
 
   const data = useStaticQuery(graphql`
@@ -24,23 +23,45 @@ export default function Layout({ children }) {
   `);
 
   return (
-    <section className="flex">
-
-      <nav id="side-nav" className={`overflow-auto flex flex-col ease-in-out duration-300 bg-blue-600 text-white h-screen  ${showSidebar ? "w-0" : "w-2/5"}`}>
-      {data.allMdx.nodes.map((node) => (
-          <Link className={`ease-in-out duration-300 p-5 m-3 mb-0 bg-gray-700 w-700 hover:bg-gray-600 ${showSidebar ? "pointer-events-none translate-x--100" : "pointer-events-auto opacity-1"}`}to={`/${node.frontmatter.slug}`} key={node.id}>
-            <span className="font-bold"> {node.frontmatter.title} </span> - {node.frontmatter.date}
+    <section className="flex h-screen">
+      <nav
+        id="side-nav"
+        className={`flex top-0 right-0 bg-blue-600 text-white fixed h-full ease-in-out duration-300 z-40 ${
+          showSidebar ? "translate-x-0" : "translate-x-3/4"
+        }`}
+      >
+        <div
+          onClick={() => setShowSidebar(!showSidebar)}
+          className="text-white transition duration-300 bg-blue-500 pr-5 pl-5 hover:bg-blue-400 hover:cursor-pointer "
+        >
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            className={`align-self-center h-screen w-5 text-center ${
+              showSidebar
+                ? "rotate-0"
+                : "rotate-180"
+            }`}
+          ></FontAwesomeIcon>
+        </div>
+        <div className="flex flex-col">
+          {data.allMdx.nodes.map((node) => (
+            <Link
+              className={`ease-in-out duration-300 p-5 m-0 mb-0 border-b-2 border-blue-500 bg-gray-700 w-700 hover:bg-gray-600 ${
+                showSidebar
+                  ? "pointer-events-auto"
+                  : "pointer-events-none opacity-0"
+              }`}
+              to={`/${node.frontmatter.slug}`}
+              key={node.id}
+            >
+              <span className="font-bold"> {node.frontmatter.title} </span> -{" "}
+              {node.frontmatter.date}
             </Link>
-      ))}
+          ))}
+        </div>
       </nav>
 
-      <div onClick={()=>setShowSidebar(!showSidebar)} className="text-white transition duration-300 bg-blue-500 pr-5 pl-5 hover:bg-blue-400 hover:cursor-pointer z-50">
-      <FontAwesomeIcon icon={faArrowRight} className={`align-self-center h-screen w-5 text-center ${showSidebar ? "rotate-0" : "rotate-180"}`}></FontAwesomeIcon>
-      </div>
-      
       {children}
-      
     </section>
-    
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
       <Layout>
-        <section className="bg-gray-500 m-10 p-20 w-full rounded">
-      <h1 className="mb-5 text-5xl text-blue-400 bg-gray-700 p-5 rounded">Welcome to My Blog!</h1>
-      <p className="text-xl text-white bg-gray-700 p-5 rounded">Please take some time and checkout some of my posts.</p>
+        <section className=" bg-gray-500 text-center md:text-left md:m-10 m-5 md:p-20 p-10 md:mr-40 mr-28 w-full rounded">
+      <h1 className=" drop-shadow mb-5 text-5xl text-blue-400 bg-gray-700 p-5 rounded">Welcome to my blog!</h1>
+      <p className=" drop-shadow text-xl text-white bg-gray-700 p-5 rounded">Please take a moment to look around and find stuff you are interested in reading.</p>
       </section>
       </Layout>
   )
