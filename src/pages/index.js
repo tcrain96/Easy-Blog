d1e3e9893cf5b18ca9fefd6d06f0867bdb228ea3
Tristan Crain <tcrain@metbiblechurch.local>
2022-10-26 10:26:43 -0400

    added padding to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
       <Layout>
-        <section className=" bg-gray-500 text-center md:text-left md:m-10 m-5 md:p-20 p-10 md:mr-40 mr-28 w-full rounded">
-      <h1 className=" drop-shadow mb-5 text-5xl text-blue-400 bg-gray-700 p-5 rounded">Welcome to my blog!</h1>
-      <p className=" drop-shadow text-xl text-white bg-gray-700 p-5 rounded">Please take a moment to look around and find stuff you are interested in reading.</p>
+        <section className=" drop-shadow-xl bg-gray-500 text-center md:text-left md:m-10 m-5 md:p-20 p-10 md:mr-40 md:mt-0 mt-20 w-full rounded">
+      <h1 className=" drop-shadow-l mb-5 text-5xl text-blue-400 bg-gray-700 p-5 rounded">Welcome to my blog!</h1>
+      <p className=" drop-shadow-l text-xl text-white bg-gray-700 p-5 rounded">Please take a moment to look around and find stuff you are interested in reading.</p>
       </section>
       </Layout>
   )
